c5aceb8b24e914b3b3af371c57052049d4d865be
Emre Can Şimşek <emrecan.midyat@gmail.com>
2023-11-01 19:59:56 +0300

    set cart page & fix product detail

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,22 +1,24 @@
 import Container from "@/app/components/Container"
-import { product } from "@/utils/product"
 import ProductDetails from "./ProductDetails"
 import ListRating from "./ListRating"
+import { products } from "@/utils/products"
 
-interface IParams{
+interface IParams {
     productId?: string
 }
 
 
-const Product = ({params}: {params: IParams}) => {
+const Product = ({ params }: { params: IParams }) => {
 
-    console.log(params)
+    const product = products.find(
+        (item) => item.id === params.productId
+    )
 
 
     return (
         <div className="p-8">
             <Container>
-                <ProductDetails product = {product} />
+                <ProductDetails product={product} />
                 <div className="flex flex-col mt-20 gap-4">
                     <div>Add Rating</div>
                     <ListRating product={product} />
