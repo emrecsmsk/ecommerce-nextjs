15c591fc62ecea3b5afa849a8731b7d206267968
Emre Can Şimşek <emrecan.midyat@gmail.com>
2023-11-01 19:09:29 +0300

    add cart page

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -1,11 +1,15 @@
 import { CartProductType } from "@/app/product/[productId]/ProductDetails";
 import { createContext, useCallback, useContext, useEffect, useState } from "react";
-import {toast} from 'react-hot-toast'
+import { toast } from 'react-hot-toast'
 
 type CartContextType = {
     cartTotalQty: number,
     cartProducts: CartProductType[] | null,
     handleAddProductToCart: (product: CartProductType) => void
+    handleRemoveProductFromCart: (product: CartProductType) => void
+    handleCartQtyIncrease: (product: CartProductType) => void
+    handleCartQtyDescrease: (product: CartProductType) => void
+    handleClearCart: () => void
 }
 
 export const CartContext = createContext<CartContextType | null>(null)
@@ -45,10 +49,95 @@ export const CartContextProvider = (props: Props) => {
         }, []
     )
 
+    const handleRemoveProductFromCart = useCallback(
+        (product: CartProductType) => {
+            if (cartProducts) {
+                const filteredProducts = cartProducts.filter(
+                    (item) => {
+                        return item.id !== product.id
+                    }
+                )
+
+                setCartProducts(filteredProducts)
+                toast.success('Product removed')
+                localStorage.setItem('eShopCartItems', JSON.stringify(filteredProducts))
+            }
+        },
+        [cartProducts]
+    )
+
+    const handleCartQtyIncrease = useCallback(
+        (product: CartProductType) => {
+            let updatedCart
+
+            if (product.quantity === 99) {
+                return toast.error(' Ooops! Maximum reached')
+            }
+
+            if (cartProducts) {
+                updatedCart = [...cartProducts]
+
+                const existingIndex = cartProducts.findIndex(
+                    (item) =>
+                        item.id === product.id
+                )
+
+                if (existingIndex > -1) {
+                    updatedCart[existingIndex].quantity = updatedCart[existingIndex].quantity + 1
+                }
+
+                setCartProducts(updatedCart)
+                localStorage.setItem('eShopCartItems', JSON.stringify(updatedCart))
+            }
+        }
+        , [cartProducts]
+    )
+
+    const handleCartQtyDescrease = useCallback(
+        (product: CartProductType) => {
+            let updatedCart
+
+            if (product.quantity === 1) {
+                return toast.error(' Ooops! Minimum reached')
+            }
+
+            if (cartProducts) {
+                updatedCart = [...cartProducts]
+
+                const existingIndex = cartProducts.findIndex(
+                    (item) =>
+                        item.id === product.id
+                )
+
+                if (existingIndex > -1) {
+                    updatedCart[existingIndex].quantity = updatedCart[existingIndex].quantity - 1
+                }
+
+                setCartProducts(updatedCart)
+                localStorage.setItem('eShopCartItems', JSON.stringify(updatedCart))
+            }
+        }
+        , [cartProducts]
+    )
+
+    const handleClearCart = useCallback(
+        () => {
+            setCartProducts(null)
+            setCartTotalQty(0)
+            localStorage.setItem('eShopCartItems', JSON.stringify(null))
+        },
+        [cartProducts],
+    )
+
+
     const value = {
         cartTotalQty,
         cartProducts,
-        handleAddProductToCart
+        handleAddProductToCart,
+        handleRemoveProductFromCart,
+        handleCartQtyIncrease,
+        handleCartQtyDescrease,
+        handleClearCart
     }
 
     return (
