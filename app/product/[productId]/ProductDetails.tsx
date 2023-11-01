1caf76c36b14be5a4d86d4d9d3ae3e6c3bb660a5
Emre Can Şimşek <emrecan.midyat@gmail.com>
2023-11-01 17:13:45 +0300

    set some add to cart logic

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -4,8 +4,11 @@ import Button from "@/app/components/products/Button"
 import ProductImage from "@/app/components/products/ProductImage"
 import SetColor from "@/app/components/products/SetColor"
 import SetQuantity from "@/app/components/products/SetQuantity"
+import { useCart } from "@/hooks/useCart"
 import { Rating } from "@mui/material"
-import { useCallback, useState } from "react"
+import { useRouter } from "next/navigation"
+import { useCallback, useEffect, useState } from "react"
+import { MdCheckCircle } from "react-icons/md"
 
 interface ProductDetailProps {
     product: any
@@ -35,6 +38,10 @@ const Horizontal = () => {
 
 const ProductDetails: React.FC<ProductDetailProps> = ({ product }) => {
 
+    const { handleAddProductToCart, cartProducts } = useCart()
+
+    const [isProductInCart, setIsProductInCart] = useState(false)
+
     const [cartProduct, setCartProduct] = useState<CartProductType>({
         id: product.id,
         name: product.name,
@@ -45,6 +52,24 @@ const ProductDetails: React.FC<ProductDetailProps> = ({ product }) => {
         quantity: 1,
         price: product.price
     })
+
+    const router = useRouter()
+
+    useEffect(() => {
+        setIsProductInCart(false)
+
+        if (cartProducts) {
+            const existingIndex = cartProducts.findIndex(
+                (item) =>
+                    item.id === product.id
+            )
+
+            if (existingIndex > -1) {
+                setIsProductInCart(true)
+            }
+        }
+    }, [cartProducts])
+
 
     const productRating = product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) / product.reviews.length
 
@@ -98,22 +123,35 @@ const ProductDetails: React.FC<ProductDetailProps> = ({ product }) => {
                 </div>
                 <div className={product.inStock ? "text-teal-400" : "text-rose-400"}>{product.inStock ? 'In stock' : 'Out of stock'}</div>
                 <Horizontal />
-                <SetColor
-                    images={product.images}
-                    cartProduct={cartProduct}
-                    handColorSelect={handleColorSelect} />
-                <Horizontal />
-                <SetQuantity
-                    cartProduct={cartProduct}
-                    handleQtyIncrease={handleQtyIncrease}
-                    handleQtyDescrease={handleQtyDescrease} />
-                <Horizontal />
-                <div className="max-w-[300px]">
-                    <Button
-                        label="Add To Cart"
-                        onClick={() => { }}
-                    />
-                </div>
+                {isProductInCart ?
+                    <>
+                        <p className="mb-2 text-slate-500 flex items-center gap-1">
+                            <MdCheckCircle className="text-teal-400" size={20} />
+                            <span>Product added to cart</span>
+                        </p>
+                        <div className="max-w-[300px]">
+                            <Button label="View Cart" outline onClick={() => { router.push("/cart") }} />
+                        </div>
+                    </>
+                    :
+                    <>
+                        <SetColor
+                            images={product.images}
+                            cartProduct={cartProduct}
+                            handColorSelect={handleColorSelect} />
+                        <Horizontal />
+                        <SetQuantity
+                            cartProduct={cartProduct}
+                            handleQtyIncrease={handleQtyIncrease}
+                            handleQtyDescrease={handleQtyDescrease} />
+                        <Horizontal />
+                        <div className="max-w-[300px]">
+                            <Button
+                                label="Add To Cart"
+                                onClick={() => handleAddProductToCart(cartProduct)}
+                            />
+                        </div>
+                    </>}
             </div>
         </div>
     )
